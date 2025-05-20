2d5e0c2ae2a5e2b7d147d2644e7b0ff81130f4e8
Atharva Gupta <atharva_gupta@berkeley.edu>
2025-05-19 19:33:04 -0700

    basic class outline + parsing working, probably needs some testing + changes to incorporate diverse input styles

---
+++ propositional_parser.py
@@ -0,0 +1,109 @@
+from abc import ABC
+from enum import Enum
+
+class ParseError(Exception):
+    pass
+
+class Sentence(ABC):
+    def evaluate(self, variable_assignment: dict):
+        raise NotImplementedError()
+
+    def __str__(self):
+        raise NotImplementedError()
+
+class Operator(Enum):
+    OR = "OR"
+    AND = "AND"
+    IMPLIES = "IMPLIES"
+    NOT = "NOT"
+
+class Atomic(Sentence):
+    def __init__(self, name):
+        self.name = name
+
+    def evaluate(self, variable_assignment: dict):
+        return variable_assignment[self.name]
+
+    def __str__(self):
+        return self.name
+
+class Negation(Sentence):
+    def __init__(self, inner):
+        super().__init__()
+        self.inner = inner
+
+    def __str__(self):
+        return f"({Operator.NOT.value} {self.inner})"
+
+    def evaluate(self, variable_assignment):
+        return not self.inner.evaluate(variable_assignment)
+
+class TwoSided(Sentence):
+    def __init__(self, left, right, oper):
+        super().__init__()
+        self.oper = oper
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return "(" + self.left.__str__() + " " + self.oper.value + " " + self.right.__str__() + ")"
+
+    def evaluate(self, variable_assignment):
+        if self.symbol == Operator.AND:
+            return self.left.evaluate(variable_assignment) and self.right.evaluate(variable_assignment)
+        if self.symbol == Operator.OR:
+            return self.left.evaluate(variable_assignment) or self.right.evaluate(variable_assignment)
+        if self.symbol == Operator.IMPLIES:
+            return (not self.left.evaluate(variable_assignment)) or self.right.evaluate(variable_assignment)
+        else:
+            raise NotImplementedError()
+
+def parse_string(s):
+    s = s.strip()
+    if s[0] == "(":
+        if s[-1] == ")":
+            s = s[1:-1]
+        else:
+            raise ParseError("Last character must be a closing parentheses!")
+
+    if len(s) == 1 and s[0] not in "\(":
+        return Atomic(s[0])
+
+    if s[0] == "\\":
+        if s[:4] != "\\not":
+            raise ParseError("Only not operand is unary!")
+
+        return Negation(parse_string(s[4:]))
+
+    count = 0
+    ind = 0
+    split = [""]
+    while ind < len(s):
+        char = s[ind]
+
+        if char == "(":
+            count += 1
+        elif char == ")":
+            count -= 1
+
+        if count == 0 and char == "\\":
+            split.append("")
+
+        if len(split) == 2 and char == " ":
+            split.append("")
+
+        split[-1] += char
+
+        ind += 1
+
+    split[1] = split[1].strip()
+
+    str_to_oper = {"\\or": Operator.OR, "\\and": Operator.AND, "\\implies": Operator.IMPLIES}
+
+    return TwoSided(parse_string(split[0]), parse_string(split[2]), str_to_oper[split[1]])
+
+def main():
+    print(parse_string("((\\not B) \implies B)"))
+
+if __name__ == "__main__":
+    main()
