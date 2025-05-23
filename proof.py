acdb5cd51c69bd87e8ac3c42031ff06312835188
Atharva Gupta <atharva_gupta@berkeley.edu>
2025-05-22 20:26:29 -0700

    added a couple of inference rules, need to work on figuring out how to incorporate true/false atomic values

--- proof.py
+++ proof.py
@@ -5,6 +5,8 @@ from typing import List
 from enum import Enum
 
 class InferenceRule(Enum):
+    axiom = "AX"
+
     and_intro = "AI"
     and_elim = "AE"
     or_intro = "OI"
@@ -15,7 +17,6 @@ class InferenceRule(Enum):
     not_elim = "NE"
 
     contra = "IP"
-    axiom = "AX"
 
 class Sequent:
     def __init__(self, gamma: List[Sentence], conclusion: Sentence, rule: InferenceRule):
@@ -52,44 +53,72 @@ class Proof:
         else:
             return False
 
-    def check_sequent(self, sequent: Sequent):
-        if sequent.rule == InferenceRule.axiom:
-            for sentence in sequent.gamma:
-                if sentence == sequent.conclusion:
+    def check_sequent(self, possible: Sequent):
+        if possible.rule == InferenceRule.axiom:
+            for sentence in possible.gamma:
+                if sentence == possible.conclusion:
                     return True
             return False
 
-        elif sequent.rule == InferenceRule.implies_elim:
+        elif possible.rule == InferenceRule.and_intro:
+            assert isinstance(possible.conclusion, TwoSided) and possible.conclusion.oper == Operator.AND
+            if self.proof_exists(possible.gamma, possible.conclusion.left) and self.proof_exists(possible.gamma, possible.conclusion.right):
+                return True
+            return False
+
+        elif possible.rule == InferenceRule.and_elim:
+            for seq in self.sequents:
+                if isinstance(seq.conclusion, TwoSided) and seq.conclusion.oper == Operator.AND:
+                    if self.gamma_equal(seq.gamma, possible.gamma):
+                        if seq.conclusion.right == possible.conclusion or seq.conclusion.left == possible.conclusion:
+                            return True
+            return False
+
+        elif possible.rule == InferenceRule.or_intro:
+            assert isinstance(possible.conclusion, TwoSided) and possible.conclusion.oper == Operator.OR
+            if self.proof_exists(possible.gamma, possible.conclusion.left) or self.proof_exists(possible.gamma, possible.conclusion.right):
+                return True
+            return False
+
+        elif possible.rule == InferenceRule.or_elim:
+            # I'll come back to this one (has to do with gamma + another formula)
+            pass
+
+        elif possible.rule == InferenceRule.implies_intro:
+            pass
+
+        elif possible.rule == InferenceRule.implies_elim:
             for seq in self.sequents:
                 if isinstance(seq.conclusion, TwoSided) and seq.conclusion.oper == Operator.IMPLIES:
-                    if self.gamma_equal(seq.gamma, sequent.gamma):
-                        if seq.conclusion.right == sequent.conclusion and self.proof_exists(seq.gamma, seq.conclusion.left):
+                    if self.gamma_equal(seq.gamma, possible.gamma):
+                        if seq.conclusion.right == possible.conclusion and self.proof_exists(seq.gamma, seq.conclusion.left):
                             return True
 
             return False
 
-        elif sequent.rule == InferenceRule.and_intro:
-            assert isinstance(sequent.conclusion, TwoSided) and sequent.conclusion.oper == Operator.AND
-            if self.proof_exists(sequent.gamma, sequent.conclusion.left) and self.proof_exists(sequent.gamma, sequent.conclusion.right):
-                return True
-            return False
+        elif possible.rule == InferenceRule.not_intro:
+            pass
+
+        elif possible.rule == InferenceRule.not_elim:
+            pass
+
+        elif possible.rule == InferenceRule.contra:
+            pass
 
         return False
 
 
 def main():
     g1 = parse_string(r"A")
-    g2 = parse_string(r"A \implies (\not A)")
-    c = parse_string(r"\not A")
-    # g1 = parse_string(r"(A \implies B)")
-    # g2 = parse_string(r"(A)")
-    # c = parse_string(r"B")
+    g2 = parse_string(r"B \or A")
+    c = parse_string(r"B")
+    # c = parse_string(r"\not A")
 
-    gamma = [g1, g2]
+    gamma = [g1]
     pr = Proof()
-    pr.add_sequent(Sequent(gamma, g1, InferenceRule.axiom))
-    pr.add_sequent(Sequent(gamma, g2, InferenceRule.axiom))
-    print(pr.add_sequent(Sequent(gamma, c, InferenceRule.implies_elim)))
+    print(pr.add_sequent(Sequent(gamma, g1, InferenceRule.axiom)))
+    print(pr.add_sequent(Sequent(gamma, g2, InferenceRule.or_intro)))
+    print(pr.add_sequent(Sequent(gamma, c, InferenceRule.or_elim)))
 
 
 if __name__ == "__main__":
